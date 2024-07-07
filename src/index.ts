8c98facdb368cff718e1c7220dcbcdfdb1d6d3ff
Matej Makara <matej.makara97@gmail.com>
2024-07-07 14:18:07 +0200

    Added Elm codegen

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { createPackageAnalyzer } from "@lit-labs/analyzer/package-analyzer.js";
-import { ModuleWithLitElementDeclarations } from "@lit-labs/analyzer/package-analyzer";
-import {} from "@lit-labs/analyzer";
 import * as path from "path";
+import * as CodeGen from "elm-codegen";
 
 const packagePath = path.resolve("../design-system-poc/src/my-element.ts");
 //@ts-ignore
@@ -10,5 +9,15 @@ const analyzer = createPackageAnalyzer(packagePath);
 //   //@ts-ignore
 //   path.resolve(packagePath, "./src/my-element.ts")
 // );
-const res = analyzer.getPackage().getLitElementModules();
-console.log(res[0].declarations[0]);
+
+const moduleDeclarations = analyzer.getPackage().getLitElementModules();
+const declarations = moduleDeclarations.flatMap((a) => a.declarations);
+
+console.log(declarations.map((a) => a));
+
+CodeGen.run("Generate.elm", {
+  debug: true,
+  output: "generated",
+  flags: {},
+  cwd: "./codegen",
+});
